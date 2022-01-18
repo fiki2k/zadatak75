fcf93f472d1c2c32a78e57395e733f86359b5be7
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-01-18 12:40:12 +0100

    Add files via upload

---
+++ zad75.c
@@ -0,0 +1,26 @@
+// Program ce izracunat volumen piramide.
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.14
+
+//Funkcija za volumen prizme.
+float volumen(int a,int h){
+	
+	//Bazu racunamo kao da je cetverostrana piramida.
+	double B= a*a;
+	float V= (1.0/3.0) *h*B;
+	return V;
+}
+
+int main(){
+   int a,h;
+   printf("Unesi duljinu strane piramide:\n");
+   scanf("%d",&a);
+   printf("Unesi visinu piramide:\n");
+   scanf("%d",&h);
+   float V=volumen(a,h);
+   printf("Povrsina baze piramide je %f\n",V);
+   return 0;
+}
